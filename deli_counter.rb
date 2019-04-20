497f04441ea84f7ac055d4ceae2b4750f4655fa8
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-20 23:43:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(katz_deli)
       puts "The line is currently empty."
     else
       katz_deli.each do |person|
-      array.push #{counter}. #{person}
+      array.push(#{counter}. #{person})
       counter += 1
     end
     puts "The line is currently: #{array.join(" ")}"
