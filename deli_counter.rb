4d786c548a7ec96eaca30b54b1e154b50e0065cd
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-20 23:28:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+    counter = 1
+    if katz_deli.length == 0
+      puts "The line is currently empty."
+    else
+      puts "#{counter}. #{katz_deli[i]}"
+      counter += 1
+  end
+end
+
