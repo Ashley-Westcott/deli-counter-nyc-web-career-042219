3f9612ce5551bfa4d9326284dd9038872e222e10
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-20 23:47:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,12 +7,16 @@ def line(katz_deli)
     counter = 1
     if katz_deli.length == 0
       puts "The line is currently empty."
-    else
-      katz_deli.each do |person|
-      array.push("#{counter}. #{person}")
-      counter += 1
+        else
+          katz_deli.each do |person|
+          array.push("#{counter}. #{person}")
+          counter += 1
+        end
+        puts "The line is currently: #{array.join(" ")}"
     end
-    puts "The line is currently: #{array.join(" ")}"
-  end
 end
 
+def take_a_number(katz_deli,new_person)
+  line.push(person)
+  puts xyz
+end  
